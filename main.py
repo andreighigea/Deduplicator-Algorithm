c1b11ba146e063391b67f9c2df8888d2441ee925
Andrei Ghigea <61598152+andreighigea@users.noreply.github.com>
2025-05-12 15:47:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 parquet_file = ''
 output_directory = ''
 
+identifier = 'company_name'
+
 # I first concluded that company names have to be unique for each company,
 # thus a redundant values would mean duplicated company data
 # some tuples have values for some attributes, and some don't
@@ -13,7 +15,7 @@ output_directory = ''
 df = pd.read_parquet(parquet_file, engine='fastparquet')
 
 # This line groups values by company name, and sets other column data to the first non None value
-filtered_df = df.groupby('company_name', as_index=False).agg(lambda x: x.dropna().iloc[0] if not x.dropna().empty else None)
+filtered_df = df.groupby(identifier, as_index=False).agg(lambda x: x.dropna().iloc[0] if not x.dropna().empty else None)
 
 # Output the new file with snappy compression
 filtered_df.to_parquet(
