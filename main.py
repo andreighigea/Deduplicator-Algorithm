173050bd054e7bf5a0cd7285725ae6d098601dbc
Andrei Ghigea <61598152+andreighigea@users.noreply.github.com>
2025-05-12 15:41:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+parquet_file = '/home/andrei/Documents/veridion_entity_resolution_challenge.snappy.parquet'
+output_directory = '/home/andrei/Documents/'
+
+# I first concluded that company names have to be unique for each company,
+# thus a redundant values would mean duplicated company data
+# some tuples have values for some attributes, and some don't
+# so I used a merge aproach where I grouped tuples by their "company_name" value
+# and merged their other values accordingly
+
+# First i read the parquet file using pandas library
+df = pd.read_parquet(parquet_file, engine='fastparquet')
+
+# This line groups values by company name, and sets other column data to the first non None value
+filtered_df = df.groupby('company_name', as_index=False).agg(lambda x: x.dropna().iloc[0] if not x.dropna().empty else None)
+
+# Output the new file with snappy compression
+filtered_df.to_parquet(
+    output_directory + 'filtered_output.parquet',
+    engine='fastparquet',
+    compression='snappy',
+    index=False
+)
